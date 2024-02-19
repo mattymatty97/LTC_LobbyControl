3d69384fcbb0de28022505a9f9120b6ccc696c4a
mattymatty <matteo.francesconi@freemails.it>
2024-02-19 16:02:36 +0100

    stick new lobby items back to the StorageCloset

---
+++ LobbyControl/Patches/CupBoardFix.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace LobbyControl.Patches
+{
+    [HarmonyPatch]
+    internal class CupBoardFix
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.Start))]
+        private static void ShipLoad1(GrabbableObject __instance)
+        {
+            GameObject closet = GameObject.Find("/Environment/HangarShip/StorageCloset");
+            MeshCollider collider = closet.GetComponent<MeshCollider>();
+            if (collider.bounds.Contains(__instance.transform.position))
+            {
+                __instance.transform.SetParent(closet.transform);
+            }
+        }
+    }
+}
