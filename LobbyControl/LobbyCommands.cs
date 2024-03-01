7d84b490ebc8832b158f92bb426f873985df9826
mattymatty <matteo.francesconi@freemails.it>
2024-03-02 00:46:04 +0100

    Patch terminal in Awake

--- LobbyControl/LobbyCommands.cs
+++ LobbyControl/LobbyCommands.cs
@@ -38,6 +38,9 @@ Extra:
         [AllowedCaller(AllowedCaller.Host)]
         public TerminalNode Lobby()
         {
+            if (!LobbyControl.Enabled)
+                return null;
+            
             var node = ScriptableObject.CreateInstance<TerminalNode>();
             node.displayText = "Invalid command:" + DefaultText;
             node.clearPreviousText = true;
@@ -50,6 +53,9 @@ Extra:
         [AllowedCaller(AllowedCaller.Host)]
         public TerminalNode Lobby([RemainingText] string text)
         {
+            if (!LobbyControl.Enabled)
+                return null;
+            
             var node = ScriptableObject.CreateInstance<TerminalNode>();
             try
             {
